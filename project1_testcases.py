d6038440bc629ab824a289be8b3858518231e2d3
Aili Hamawaki <ailihamawaki@Ailis-MacBook-Air.local>
2025-10-15 13:13:23 -0400

    Add imports for penguins analysis functions in unit test setup

---
+++ project1_testcases.py
@@ -0,0 +1,10 @@
+import unittest
+from penguins_analysis import (  # replace with your filename (without .py)
+    avg_body_mass_by_species_and_sex,
+    avg_bill_length_by_island_and_year,
+    avg_flipper_length_by_species_and_island,
+    body_mass_difference_by_sex_and_island,
+    bill_depth_vs_flipper_length_by_species,
+    avg_bill_length_by_year_and_sex
+)
+
